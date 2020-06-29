8629ff410cbf197488e67dbd4c028bda95dbb2cd
Acapellia <longchim@naver.com>
2020-06-30 08:52:01 +0900

    200630_homework

---
+++ 수업/C++/200629_homework/200629_homework/Avanced.cpp
@@ -0,0 +1,88 @@
+#include<iostream>
+#include <cstdlib>
+#include <string> 
+#include<cstring>
+using namespace std;
+//1. string을 이용하여 영문 한줄 입력받고 거꾸로 출력할 것
+//Reversestring
+void problem1() {
+	string str;
+	cout << "문자열을 입력하세요 : ";
+	cin >> str;
+	for (int i = str.length()-1; i >= 0; i--)
+		cout << str[i];
+}
+//2. Histogram 클래스 만들것
+
+//#include <iostream>
+//#include <string>
+//using namespace std;
+
+//int main() {
+//	Histogram elvisHisto("Wise men say, only fools rush in But I can't help, \n");
+//	elvisHisto.put("falling in love with you");
+//	elvisHisto.putc('-');
+//	elvisHisto.put("Elvis Presley");
+//	elvisHisto.print();
+//}
+class Histogram {
+private:
+	string str;
+public:
+	Histogram() {}
+	Histogram(string _str) {
+		str = _str;
+	}
+	void put(string _str) {
+		str += _str;
+	}
+	void putc(char c) {
+		str += c;
+	}
+	void print() {
+		cout << str << endl;
+	}
+	void showHistogram() {
+		int alpha[27] = {0};
+		for (int i = 0; i < str.length(); i++) {
+			if (str[i] >= 'A' && str[i]<='Z') str[i] = str[i] + ('a' - 'A');
+			if (str[i] >= 'a' && str[i] <= 'z') {
+				int idx = (str[i] - 'a')+1 ;
+				alpha[idx]++;
+			}
+		}
+		
+		for (int i = 1; i <= 26; i++) {
+			cout << (char)('a' + (i - 1)) <<" ("<< alpha[i] << ")      : ";
+			for (int j = 0; j < alpha[i]; j++) {
+				cout << "*";
+			}
+			cout << endl;
+		}
+	}
+};
+void problem2() {
+	Histogram elvisHisto("Wise men say, only fools rush in But I can't help, \n");
+	elvisHisto.put("falling in love with you");
+	elvisHisto.putc('-');
+	elvisHisto.put("Elvis Presley");
+	elvisHisto.print();
+	elvisHisto.showHistogram();
+}
+//3.TelInfo의 클래스내의 이름과 전화번호를 동적할당하시오.
+//동적할당할 때 생성자와 소멸자를 이용할 것.
+//
+//void problem3() {}
+int main()
+{
+	while (1) {
+		int sel;
+		cout << "원하는 과제를 입력하세요 : ";
+		cin >> sel;
+		if (sel == 0) { cout << "end!!" << endl; break; }
+		else if (sel == 1) { problem1(); }
+		else if (sel == 2) { problem2(); }
+		cout << endl;
+	}
+	return 0;
+}
---
+++ 수업/C++/200629_homework/200629_homework/Middle.cpp
@@ -0,0 +1,128 @@
+#include<iostream>
+#include <cstdlib>
+#include <ctime> 
+#include<cstring>
+using namespace std;
+//1. cout << 연산자를 이용해서 구구단을 출력하세요
+//2 x 1 = 2  3 x 1 = 3 ..... 9 x 1 = 9
+//......
+//2 x 9 = 18
+void problem1() {
+	for (int i = 1; i <= 9; i++) {
+		for (int j = 2; j <= 9; j++) {
+			cout << j << " * " << i << " = " << j * i << " ";
+		}cout << endl;
+	}
+}
+//2. 키보드로부터 2개의 정수를 읽어서 큰 수를 출력하세요
+void problem2() {
+	int num1, num2;
+	cout << "2개의 정수를 입력하세요 : ";
+	cin >> num1 >> num2;
+	cout << (num1 > num2 ? num1 : num2);
+}
+//3. 소수점을 가지는 5개의 실수를 입력받아 제일 큰 수를 출력하세요
+void problem3() {
+	double max = 0.0;
+	cout << "5개의 실수를 입력하세요"<<endl;
+	for (int i = 1; i <= 5; i++) {
+		cout << i << "번째 실수를 입력하세요 : ";
+		double num; cin >> num;
+		max < num ? (max = num) : (max = max);
+	}
+	cout << "max : " << max<<endl;
+
+}
+//4. < Enter > 키가 입력될 때까지 문자들을 읽고, 입력된 문자 'x'의
+//개수를 화면에 출력하세요.
+void problem4() {
+	getchar();
+	int cnt = 0;
+	while (true) {
+		char x;
+		cin.get(x);
+		if (x == '\n') { cout << "cnt : " << cnt; break; }
+		cnt++;
+	}
+}
+//5. "yes"가 입력될 때까지 종료하지 않는 프로그램을 작성해세요.입력은 cin.getline() 함수를 사용하세요
+// 이상함
+void problem5() {
+	getchar();
+	while (1) {
+		char x[10];
+		cout << "문자열을 입력하세요 : ";
+		//getchar();
+		cin.getline(x, 10);
+		//cin.getline(x, strlen(x),'/n');
+		//cout << x << endl;
+		//cout << "len : " << strlen(x) << endl;
+		if (!strcmp(x, "yes")) { cout << "종료" << endl; break; }
+		//cout << "string : " << x<<endl;
+	}
+}
+//6. ';'으로 5개의 이름을 구분해서 입력받아, 각 이름을 끊어내여 화면에 출력하세요.
+//그리고 가장 긴 이름을 판별해서 나타내세요.
+//cin.getline(str, 100, ';');
+//홍길동; 임꺽정; 장길산; 일지매; 모짜르트;
+//1:홍길동
+//2 : 임꺽정
+//3 : 장길산
+//4 : 일지매
+//5 : 모짜르트
+void problem6() {
+	getchar();
+	int len=0;
+	for (int i = 0; i < 5; i++) {
+		char str[100] = { 0 };
+		cin.getline(str, 100, ';');
+		if (len < strlen(str)) len = strlen(str);
+		cout << i+1 << " : "  << str << endl;
+	}
+	cout << endl;
+	cout << "len : " << len << endl;
+}
+//7. 이름, 주소, 나이를 입력받아 출력하세요.
+void problem7() {
+	char name[20], addr[20];
+	int age;
+	cout << "이름 : ";  cin >> name; cout << name << endl;
+	cout << "주소 : ";  cin >> addr; cout << addr << endl;
+	cout << "나이 : ";  cin >> age; cout << age << endl;
+
+}
+//8. 문자열을 하나 입력받고 문자열의 부분 문자열을 출력하세요
+//hello
+//h
+//he
+//hel
+//hell
+//hello
+void problem8() {
+	char str[20];
+	cin >> str;
+	for (int i = 0; i < strlen(str); i++) {
+		for (int j = 0; j <= i; j++) {
+			cout << str[j];
+		}cout << endl;
+	}
+}
+int main()
+{
+	while (1) {
+		int sel;
+		cout << "원하는 과제를 입력하세요 : ";
+		cin >> sel;
+		if (sel == 0) { cout << "end!!" << endl; break; }
+		else if (sel == 1) { problem1(); }
+		else if (sel == 2) { problem2(); }
+		else if (sel == 3) { problem3(); }
+		else if (sel == 4) { problem4(); }
+		else if (sel == 5) { problem5(); }
+		else if (sel == 6) { problem6(); }
+		else if (sel == 7) { problem7(); }
+		else if (sel == 8) { problem8(); }
+		cout << endl;
+	}
+	return 0;
+}
