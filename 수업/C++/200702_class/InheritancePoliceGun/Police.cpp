43829661a663ebfa4a91328ef357c5c565971ef9
Acapellia <longhchim@naver.com>
2020-07-06 15:32:56 +0900

    ignore test2

--- 수업/C++/200702_class/InheritancePoliceGun/Police.cpp
+++
@@ -1,7 +0,0 @@
-#include "Police.h"
-void Police::wearSuit() {
-	hat = "Hat";
-	suit = "Suit";
-	cout << hat << endl;
-	cout << suit << endl;
-}
--- 수업/C/omok/day4-team02.c
+++
@@ -1,9 +0,0 @@
-#define PAN_SIZE 19
-
-int team02(int pan[][PAN_SIZE], int* px, int* py)
-{
-	*px = rand() % PAN_SIZE;
-	*py = rand() % PAN_SIZE;
-	return 0;
-}
-
