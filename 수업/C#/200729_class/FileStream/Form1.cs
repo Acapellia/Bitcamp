e6b7bc5a569ec1970351f35bdd005b23eb7df25d
Acapellia <longhchim@naver.com>
2020-07-30 08:58:59 +0900

    200729_class

---
+++ 수업/C#/200729_class/FileStream/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+namespace FileStream {
+    public partial class Form1 : Form {
+        public Form1() {
+            InitializeComponent();
+        }
+
+        private void btnSaveFile_Click(object sender, EventArgs e) {
+            SaveFileDialog sfd = new SaveFileDialog();
+            if(sfd.ShowDialog() == DialogResult.OK) {
+                string path = sfd.FileName;
+                byte[] data = { 65, 66, 67, 68, 69, 70, 71, 72, 73, 74 };
+                System.IO.FileStream fs = new System.IO.FileStream(path, FileMode.Create, FileAccess.Write);
+            }
+        }
+
+        private void btnOpenFile_Click(object sender, EventArgs e) {
+            OpenFileDialog ofd = new OpenFileDialog();
+            if(ofd.ShowDialog() == DialogResult.OK) {
+                string path = ofd.FileName;
+                byte[] data = new byte[10];
+                System.IO.FileStream fs = new System.IO.FileStream(path, FileMode.Open, FileAccess.Read);
+                fs.Read(data, 0, data.Length);
+                fs.Close();
+                string result = "";
+                foreach(byte b in data) {
+                    result += b.ToString();
+                }
+                MessageBox.Show(result);
+            }
+        }
+    }
+}
---
+++ 수업/C#/200729_class/_40_Serialize/Form1.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization.Formatters.Soap;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _40_Serialize {
+    public partial class Form1 : Form {
+        Human human = new Human();
+        public Form1() {
+            InitializeComponent();
+            this.Load += Form1_Load;
+        }
+
+        private void Form1_Load(object sender, EventArgs e) {
+            human.Name = "홍길동";
+            human.Age = 24;
+            human.Height = 158;
+            human.Weight = 48;
+        }
+
+        private void btnReadSerialize_Click(object sender, EventArgs e) {
+            DeserialzeXML();
+        }
+
+        private void btnWriteSerialize_Click(object sender, EventArgs e) {
+            SerialzeXML();
+        }
+
+        void SerializeBinary() {
+            FileStream fs = new FileStream("Human.bin", FileMode.Create, FileAccess.Write);
+            BinaryFormatter bf = new BinaryFormatter();
+            bf.Serialize(fs, human);
+            fs.Close();
+        }
+        void SerialzeXML() {
+            FileStream fs = new FileStream("Human.xml", FileMode.Create, FileAccess.Write);
+            SoapFormatter bf = new SoapFormatter();
+            bf.Serialize(fs, human);
+            fs.Close();
+        }
+        void DeserializeBinary() {
+            FileStream fs = new FileStream("Human.bin", FileMode.Open, FileAccess.Read);
+            BinaryFormatter bf = new BinaryFormatter();
+            Human hong = bf.Deserialize(fs) as Human;
+            MessageBox.Show(hong.ToString(), "Human Information");
+            fs.Close();
+        }
+        void DeserialzeXML() {
+            FileStream fs = new FileStream("Human.xml", FileMode.Open, FileAccess.Read);
+            SoapFormatter bf = new SoapFormatter();
+            Human hong = bf.Deserialize(fs) as Human;
+            MessageBox.Show(hong.ToString(), "Human Information");
+            fs.Close();
+        }
+    }
+}
